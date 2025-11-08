3eaf29e9c04c2a32e6c199f31c073f4650213034
ppljc <tony_shmidt@mail.ru>
2025-11-08 11:05:36 +0600

    feat: oop lab5 program.cs

--- ООП/lab5/lab5/Program.cs
+++ ООП/lab5/lab5/Program.cs
@@ -1,3 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace lab5;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Лабораторная работа 5");
+            Console.WriteLine("Задачи:");
+            Console.WriteLine("1 - Файл списка группы");
+            Console.WriteLine("2 - Сканер изображений на дисках");
+            Console.WriteLine("0 - Выход");
+            Console.Write("\nВведите номер задачи: ");
+
+            string input = Console.ReadLine();
+            Console.Clear();
+
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("Задача 1: Файл списка группы\n");
+                    Task1.Execute();
+                    break;
+                
+                case "2":
+                    Console.WriteLine("Задача 2: Сканер изображений на диске\n");
+                    Task2.Execute();
+                    break;
+                
+                case "0":
+                    Console.WriteLine("Выход...");
+                    return;
+
+                default:
+                    Console.WriteLine("Неверный номер задачи.");
+                    break;
+            }
+
+            Console.WriteLine("\nНажмите любую клавишу, чтобы продолжить...");
+            Console.ReadKey();
+        }
+    }
+}
