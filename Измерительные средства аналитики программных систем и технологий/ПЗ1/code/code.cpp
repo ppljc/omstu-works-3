98b61e4bd801f479aac591c4f00892b20f6d41b3
Антон <tony_shmidt@mail.ru>
2025-12-19 05:55:52 +0600

    feat: analytics practice 6 completed

--- Измерительные средства аналитики программных систем и технологий/ПЗ1/code/code.cpp
+++ Измерительные средства аналитики программных систем и технологий/ПЗ1/code/code.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
 #include <string>
 #include <cctype>
-
 using namespace std;
-
 int main() {
     string input;
     cout << "Enter a string (up to 100 characters): ";
     getline(cin, input);
-
     if (!input.empty() && input.back() == '\n') {
         input.pop_back();
     }
-
     int length = input.length();
     int vowels = 0;
     int freq[26] = {0};
     char most_common = '\0';
     int max_count = 0;
-
     string vowel_chars = "aeiouyAEIOUY";
-
     for (char c : input) {
         if (isalpha(c)) {
             if (vowel_chars.find(c) != string::npos) {
                 vowels++;
             }
-
             char lower = tolower(c);
             int index = lower - 'a';
             freq[index]++;
@@ -36,10 +29,8 @@ int main() {
             }
         }
     }
-
     cout << "String length: " << length << '\n';
     cout << "Number of vowels: " << vowels << '\n';
-
     if (max_count > 0) {
         cout << "Most frequent letter: '" << most_common 
              << "' (appears " << max_count << " time";
@@ -48,9 +39,7 @@ int main() {
     } else {
         cout << "Most frequent letter: (no letters)" << '\n';
     }
-
     cout << "\nPress Enter to exit...";
     cin.get();
-
     return 0;
 }
